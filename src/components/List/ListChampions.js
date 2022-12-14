24f8334676bc9237690b3702b256e272085002b7
Victor Prudhomme <contact@victor-prdh.com>
2022-12-14 09:02:00 +0100

    fix: typo

--- src/components/List/ListChampions.js
+++ src/components/List/ListChampions.js
@@ -32,7 +32,7 @@ export function ListChampions({ champions }) {
                     : 
                         <View style={{width: '100%', height: '100%', justifyContent: 'center'}}>
                             <Image source={require('../../../assets/not-found.png')} style={{maxWidth: '80%', height: 360, alignSelf: 'center', resizeMode: 'cover'}} />
-                            <Title additionalStyle={{color: 'black', textAlign: 'center'}}>Aucun champion aimé</Title>
+                            <Title additionalStyle={{color: 'black', textAlign: 'center'}}>Aucun champion trouvé</Title>
                         </View>
             }
         </>
